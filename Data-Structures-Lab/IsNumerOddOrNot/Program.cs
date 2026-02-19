60a1e30b21f6bc07d8406fad74a8c1a26a1a1da6
Anthony Suárez Cardona <anthonysuarez336@gmail.com>
2026-02-19 11:14:36 -0500

    Console Extension

--- Data-Structures-Lab/IsNumerOddOrNot/Program.cs
+++ Data-Structures-Lab/IsNumerOddOrNot/Program.cs
@@ -1,29 +1,28 @@
-﻿var numberString = string.Empty;
+﻿using Shared;
+
+var answer = string.Empty;
+var options = new List<string> { "s" , "n" };
+
 do
-{
-    Console.Write("Ingrese número entero o la palabra 'Salir' para salir:");
-    numberString = Console.ReadLine();
-    if (numberString!.ToLower() == "salir")
+{ 
+    var number = ConsoleExtension.GetInt("Ingrese número entero diferente de cero: ");
+    if (number == 0)
     {
         continue;
     }
-
-    var numberInt = 0;
-    if (int.TryParse(numberString, out numberInt))
-    {
-        if (numberInt % 2 == 0)
-        {
-            Console.WriteLine($"El número {numberInt}, es par.");
-        }
-        else
-        {
-            Console.WriteLine($"El número {numberInt} es impar.");
-        }
+    if(number % 2 == 0)
+    { 
+        Console.WriteLine($"El número {number}, es par.");
     }
     else
     {
-        Console.WriteLine($"Lo que ingresaste: {numberString}, no es un número entero.");
+        Console.WriteLine($"El número {number} es impar.");
     }
+    do
+    {
+        answer = ConsoleExtension.GetValidOptions("¿Deseas continuar [S]í, [N]o?: ", options);
+    } while (!options.Any(x => x.Equals(answer, StringComparison.CurrentCultureIgnoreCase)));
 
-} while (numberString!.ToLower() != "salir");
+} while (answer!.Equals("s", StringComparison.CurrentCultureIgnoreCase));
+
 Console.WriteLine("Game Over");
