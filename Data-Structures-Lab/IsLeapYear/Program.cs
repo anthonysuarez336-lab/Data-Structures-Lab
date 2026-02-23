c4b436c12a2715d4ebac7f6ed4592b5d48026e80
Anthony Suárez Cardona <anthonysuarez336@gmail.com>
2026-02-22 21:04:34 -0500

    Is Leap year

---
+++ Data-Structures-Lab/IsLeapYear/Program.cs
@@ -0,0 +1,45 @@
+﻿using Shared;
+
+do
+{
+    var currrentYear = DateTime.Now.Year;
+    var message = string.Empty;
+    var year = ConsoleExtension.GetInt("Ingrese el año : ");
+
+    if (year == currrentYear)
+    {
+        message = "es";
+    }   else if (year > currrentYear)
+    { 
+        message = "va a ser";
+    }
+    else
+    {
+        message = "fue";
+    }
+
+
+    if (year % 4 == 0)
+    {
+        if (year % 100 == 0)
+        {
+            if (year % 400 == 0)
+            {
+                Console.WriteLine($"El año: {year}, SI {message} bisiesto.");
+            }
+            else
+            {
+                Console.WriteLine($"El año: {year}, NO {message} bisiesto.");
+            }
+
+        }
+        else
+        {
+        Console.WriteLine($"El año: {year}, SI {message} bisiesto.");
+        }
+    }
+    else
+    {
+        Console.WriteLine($"El año: {year},No {message} bisiesto.");
+    }
+} while (true);
