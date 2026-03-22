9dd10f29d21c5b27133c7d107cd093ff767f55df
Anthony Suárez Cardona <anthonysuarez336@gmail.com>
2026-03-21 22:29:27 -0500

    Shipping

--- Data-Structures-Lab/Shared/ConsoleExtension.cs
+++ Data-Structures-Lab/Shared/ConsoleExtension.cs
@@ -48,6 +48,7 @@ public static class ConsoleExtension
         return text;
     }
 
+    
     public static string? GetValidOptions(string message, List<string> options)
     {
         Console.Write(message);
